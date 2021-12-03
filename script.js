61608cb4b570dd61fbd194fe5711a228b927f177
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-03 20:12:19 -0300

    add name in Headding

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const callPokemons = () =>  fetch('https://pokeapi.co/api/v2/pokemon/')
   .then((data) => {
   const pokemonsURL = data.results.filter(pokemon => pokemon.name === 'pidgeotto')[0]
   console.log(pokemonsURL.name)
+  const pokeDiv = document.querySelector('#root')
+  const headding = document.createElement('h1')
+  headding.innerHTML = pokemonsURL.name
+  headding.style.marginBottom = '60px'
+  pokeDiv.appendChild(headding)
   fetch(pokemonsURL.url).then(res2 => res2.json())
     .then(data2=> {
       console.log(data2.sprites.front_default)
       const pokeIMG = document.createElement('img')
       pokeIMG.src = data2.sprites.front_default
       pokeIMG.style.scale = 2
-      const pokeDiv = document.querySelector('#root')
       pokeDiv.appendChild(pokeIMG)
     })
 })
