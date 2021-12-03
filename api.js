a020786de3972c5b2847f9da25a2b3106b0874f9
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-03 19:39:28 -0300

    finish

---
+++ api.js
@@ -0,0 +1,20 @@
+const callPokemons = () =>  fetch('https://pokeapi.co/api/v2/pokemon/')
+  .then((res) => res.json())
+  .then((data) => {
+  const pokemonsURL = data.results.filter(pokemon => pokemon.name === 'pidgeotto')[0].url
+  fetch(pokemonsURL).then(res2 => res2.json())
+    .then(data2=> {
+      console.log(data2.sprites.front_default)
+      const pokeIMG = document.createElement('img')
+      pokeIMG.src = data2.sprites.front_default
+      pokeIMG.style.scale = 2
+      const pokeDiv = document.querySelector('#root')
+      pokeDiv.appendChild(pokeIMG)
+    })
+    
+  console.log(pokeIMG);
+
+})
+
+callPokemons()
+
