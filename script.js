00aa2bfcdd493899105d8c99233058f68e49b65c
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-03 21:56:12 -0300

    add image back and show on click with addEventList

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-const callPokemons = () =>  fetch('https://pokeapi.co/api/v2/pokemon/')
+const insertPokemonPidgeott = () =>  fetch('https://pokeapi.co/api/v2/pokemon/')
   .then((res) => res.json())
   .then((data) => {
-  const pokemonsURL = data.results.filter(pokemon => pokemon.name === 'pidgeotto')[0]
-  console.log(pokemonsURL.name)
-  const pokeDiv = document.querySelector('#root')
-  const headding = document.createElement('h1')
-  headding.innerHTML = pokemonsURL.name
-  headding.style.marginBottom = '60px'
-  pokeDiv.appendChild(headding)
-  fetch(pokemonsURL.url).then(res2 => res2.json())
-    .then(data2=> {
-      console.log(data2.sprites.front_default)
-      const pokeIMG = document.createElement('img')
-      pokeIMG.src = data2.sprites.front_default
-      pokeIMG.style.scale = 2
-      pokeDiv.appendChild(pokeIMG)
-    })
-})
+  const pokemonPidgeott = data.results
+    .filter(pokemon => pokemon.name === 'pidgeotto')[0]
+    
+    console.log(pokemonPidgeott.name)
+    const pokeDiv = document.querySelector('#root')
+    const headding = document.createElement('h1')
+    headding.innerHTML = pokemonPidgeott.name
+    headding.style.marginBottom = '60px'
+    pokeDiv.appendChild(headding)
 
-callPokemons()
+    fetch(pokemonPidgeott.url)
+      .then(resPokemon => resPokemon.json())
+      .then(dataPokemon=> {
+        console.log(dataPokemon.sprites.front_default)
+        const pokeIMG = document.createElement('img')
+        pokeIMG.src = dataPokemon.sprites.front_default
+        pokeIMG.addEventListener('mousedown', () => pokeIMG.src = dataPokemon.sprites.back_default)
+        pokeIMG.addEventListener('mouseup', () => pokeIMG.src = dataPokemon.sprites.front_default)
+        pokeIMG.style.scale = 2
+        pokeDiv.appendChild(pokeIMG)
+
+        const paragrathClick = document.createElement('p')
+        paragrathClick.innerHTML = 'click on the image'
+        paragrathClick.style.marginTop = '60px'
+        pokeDiv.appendChild(paragrathClick)
+      })
+  })
+
+  insertPokemonPidgeott()
 
